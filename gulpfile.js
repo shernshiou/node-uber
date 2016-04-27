ff7d2903fc849868bcc12118d0c47c578db302db
Alexander Graebe <alex.graebe@gmail.com>
2016-04-27 14:23:34 -0700

    small style updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp')
-  , mocha = require('gulp-mocha');
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
-    return gulp.src('test/index.js', { read: false })
-        .pipe(mocha({reporter: 'spec'}));
+gulp.task('test', function() {
+    return gulp.src(['test/**.js'], {
+            read: false
+        })
+        .pipe(mocha({
+            reporter: 'spec'
+        }));
 });
 
 gulp.task('default', ['test']);
